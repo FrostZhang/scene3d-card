b4c9742bd110d72d9ee3a3bec1ada290db5f2157
Frostzhang <376237519@qq.com>
2022-04-18 18:14:02 +0800

    配置化

---
+++ Assets/Scripts/House/HassEntity.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HassEntity : MonoBehaviour
+{
+    public string entity_id;
+    const string switchkey = "switch";//on off
+    const string lightkey = "light";  //on off
+    const string climate = "climate"; // on off
+    const string binary_sensor = "binary_sensor"; //on off
+    const string media_player = "media_player"; //idle
+    const string person = "person"; //not_home
+    const string cover = "cover"; //´°Á± closed closing open opening
+    const string fan = "fan"; //on off
+    const string on = "on";
+    const string off = "off";
+
+    string head;
+    const string cmdon = "turn_on";
+    const string cmdoff = "turn_off";
+    protected bool open = false;
+    public void SetEntity(string id)
+    {
+        if (string.IsNullOrEmpty(id))
+            return;
+        var ids = id.Split('.');
+        if (ids.Length == 2)
+        {
+            head = ids[0];
+            entity_id = id;
+        }
+    }
+
+    public virtual void Click()
+    {
+        if (string.IsNullOrEmpty(entity_id))
+            return;
+        HassMessage message = new HassMessage();
+        message.head = head;
+        message.entity_id = entity_id;
+        if (open)
+        {
+            open = false;
+            message.cmd = cmdoff;
+        }
+        else
+        {
+            open = true;
+            message.cmd = cmdon;
+        }
+        Shijie.LightMessage(JsonUtility.ToJson(message));
+    }
+
+    public virtual void LongClick()
+    {
+        if (string.IsNullOrEmpty(entity_id))
+            return;
+    }
+}
---
+++ Assets/Scripts/House/House.cs
@@ -0,0 +1,165 @@
+using LitJson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+    Dictionary<string, Color> colors;
+
+    void Awake()
+    {
+        colors = new Dictionary<string, Color>(10);
+        colors.Add("red", Color.red);
+        colors.Add("grey", Color.grey);
+        colors.Add("gray", Color.gray);
+        colors.Add("yellow", Color.yellow);
+        colors.Add("magenta", Color.magenta);
+        colors.Add("cyan", Color.cyan);
+        colors.Add("black", Color.black);
+        colors.Add("white", Color.white);
+        colors.Add("blue", Color.blue);
+        colors.Add("green", Color.green);
+    }
+
+    void Start()
+    {
+        JsonWriter w = new JsonWriter();
+        w.WriteObjectStart();
+        w.WritePropertyName("ceshi");
+        w.Write(213);
+        w.WriteObjectEnd();
+        Debug.Log(w.ToString());
+        CreatWall("2.5,0,5,1,0.125,90", null);
+        CreatWall("0,2.5,5.25,0.5,0.25,0", null);
+        CreatWall("-2.5,0,5,1,0.125,90", null);
+        CreatWall("0,-2.5,5.25,0.5,0.25,0", null);
+        CreatFloor("8,6", null);
+    }
+
+    public void CreatFloor(string floor, string color)
+    {
+        var ss = floor.Split(',');
+        float va;
+        if (ss.Length == 2)
+        {
+            float x, y;
+            if (float.TryParse(ss[0], out va))
+                x = va;
+            else return;
+            if (float.TryParse(ss[1], out va))
+                y = va;
+            else return;
+            var plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            plane.transform.localScale = new Vector3(x * 0.1f, 1, y * 0.1f);
+            Destroy(plane.GetComponent<Collider>());
+            var r = plane.GetComponent<Renderer>();
+            var ma = r.material = new Material(r.material);
+            Color wcolor;
+            if (TryColor(color, out wcolor))
+            {
+                ma.color = wcolor;
+            }
+        }
+    }
+
+    public void CreatWall(string wall, string color)
+    {
+        var ws = wall.Split(',');
+        float va;
+        if (ws.Length == 6)
+        {
+            float x, y, w, h, t, a;
+            if (float.TryParse(ws[0], out va))
+                x = va;
+            else return;
+            if (float.TryParse(ws[1], out va))
+                y = va;
+            else return;
+            if (float.TryParse(ws[2], out va))
+                w = va;
+            else return;
+            if (float.TryParse(ws[3], out va))
+                h = va;
+            else return;
+            if (float.TryParse(ws[4], out va))
+                t = va;
+            else return;
+            if (float.TryParse(ws[5], out va))
+                a = va;
+            else return;
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.localPosition = new Vector3(x, h * 0.5f, y);
+            cube.transform.localScale = new Vector3(w, h, t);
+            cube.transform.localEulerAngles = new Vector3(0, a, 0);
+            Destroy(cube.GetComponent<Collider>());
+            var r = cube.GetComponent<Renderer>();
+            var ma = r.material = new Material(r.material);
+            Color wcolor;
+            if (TryColor(color, out wcolor))
+            {
+                ma.color = wcolor;
+            }
+        }
+    }
+
+    protected bool TryColor(string cstr, out Color va)
+    {
+        if (string.IsNullOrWhiteSpace(cstr))
+        {
+
+        }
+        else if (cstr.StartsWith("#"))
+        {
+            return ColorUtility.TryParseHtmlString(cstr, out va);
+        }
+        else if (colors.ContainsKey(cstr))
+        {
+            va = colors[cstr];
+            return true;
+        }
+        else
+        {
+            return RGBAStr2Color(cstr, out va);
+        }
+        va = Color.magenta;
+        return false;
+
+    }
+
+    bool RGBAStr2Color(string str, out Color color)
+    {
+        var css = str.Split(',');
+        byte va;
+        if (css.Length == 4)
+        {
+            byte r, g, b, a;
+            if (byte.TryParse(css[0], out va))
+                r = va;
+            else goto end;
+            if (byte.TryParse(css[1], out va))
+                g = va;
+            else goto end;
+            if (byte.TryParse(css[2], out va))
+                b = va;
+            else goto end;
+            if (byte.TryParse(css[3], out va))
+                a = va;
+            else goto end;
+            color = new Color32(r, g, b, a);
+            return true;
+        }
+    end:
+        color = Color.magenta;
+        return false;
+    }
+}
+
+public class HouseConfigData
+{
+    public class Wall
+    {
+        public string chicun;
+        public string color;
+    }
+}
